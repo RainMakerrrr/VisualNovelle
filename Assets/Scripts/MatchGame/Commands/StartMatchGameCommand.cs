940c1ada638c3f9d1feb85a13cb38c37f4e4ac4d
Alexandr <zakharov.a@azurgames.com>
2024-11-12 22:26:56 +0300

    implement custom command for match game loading

---
+++ Assets/Scripts/MatchGame/Commands/StartMatchGameCommand.cs
@@ -0,0 +1,18 @@
+﻿using Naninovel;
+using UnityEngine.SceneManagement;
+
+namespace MatchGame.Commands
+{
+    [CommandAlias("startMatchGame")]
+    public class StartMatchGameCommand : Command
+    {
+        private const string FindMatchGame = "FindMatch";
+
+        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+        {
+            SceneManager.LoadScene(FindMatchGame);
+            
+            return UniTask.CompletedTask;
+        }
+    }
+}
